e2db96326288e76f72729b302564edc79bc1ca48
Antoine CHOULA <76167680+kevAnto@users.noreply.github.com>
2025-05-02 09:58:08 -0400

    rm ps sso

--- providers.tf
+++ providers.tf
@@ -1,10 +1,12 @@
-provider "aws" {
-  region = var.region
-
-  default_tags {
-    tags = {
-      created_by = "terraform"
-      workspace = terraform.workspace
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.52.0"
     }
   }
+}
+
+provider "aws" {
+  region = var.region
 }
--- variables.tf
+++ variables.tf
@@ -3,15 +3,3 @@ variable "region" {
   type        = string
   default     = "us-east-1"
 }
-
-variable "permissions_list" {
-  type = list(object({
-    name             = string
-    description      = string
-    session_duration = string
-    managed_policies = list(string)
-    aws_accounts     = list(string)
-    sso_groups       = list(string)
-  }))
-  description = "List of permission set properties"
-}
