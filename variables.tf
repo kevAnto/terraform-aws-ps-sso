f7489dac6c2b4649344ff88ced7f6fec48f32271
Antoine CHOULA <76167680+kevAnto@users.noreply.github.com>
2025-05-05 00:04:05 -0400

    update

--- variables.tf
+++ variables.tf
@@ -1,5 +1,28 @@
+<<<<<<< Updated upstream
 variable "vpc_cidr_block" {}
 variable "subnet_cidr_block" {}
 variable "avail_zone" {}
 variable "env_prefix" {}
 variable "instance_type" {}
+=======
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  default     = "us-east-1"
+}
+
+variable "environment" {
+  description = "Environment name"
+  default     = "prod"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  default     = "10.0.0.0/16"
+}
+
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
+>>>>>>> Stashed changes
